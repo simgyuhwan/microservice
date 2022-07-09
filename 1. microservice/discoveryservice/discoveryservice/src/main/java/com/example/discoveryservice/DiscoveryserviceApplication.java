d60124205517f3932a80e7b86697f873dc514975
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2022-07-09 22:01:00 +0900

    .

--- 1. microservice/discoveryservice/discoveryservice/src/main/java/com/example/discoveryservice/DiscoveryserviceApplication.java
+++ 1. microservice/discoveryservice/discoveryservice/src/main/java/com/example/discoveryservice/DiscoveryserviceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DiscoveryserviceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryserviceApplication.class, args);
 	}
