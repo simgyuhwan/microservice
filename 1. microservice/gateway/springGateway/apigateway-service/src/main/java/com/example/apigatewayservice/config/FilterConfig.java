385e016827b191fef370952f1504d417ec0acc84
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2022-07-16 20:26:04 +0900

    filter

--- 1. microservice/gateway/springGateway/apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
+++ 1. microservice/gateway/springGateway/apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -1,2 +1,26 @@
-package com.example.apigatewayservice.config;public class FilterConfig {
+package com.example.apigatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    // yml 에서 하던 것을 자바코드에서 할 수 있다.
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r-> r.path("/first-service/**")
+                        .filters(f -> f.addRequestHeader("first-request","first-request-header")
+                                       .addResponseHeader("first-response","first-response-header"))
+                        .uri("http://localhost:8081"))
+                .route(r -> r.path("/second-service/**")
+                        .filters(f -> f.addRequestHeader("second-request", "second-request-header")
+                                .addResponseHeader("second-response", "second-response-header"))
+                                .uri("http://localhost:8082"))
+                .build();
+    }
+
 }
